c1b7b4442686268dcb467f153e61f5a0c761a1fd
Lintaaa-25 <lintababu3@gmail.com>
2025-05-09 19:36:42 +0530

    Create App.tsx

---
+++ smarter-tasks/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TaskApp from "./TaskApp";
+import "./index.css"; // Tailwind CSS entry
+
+function App() {
+  return (
+    <div className="App">
+      <TaskApp />
+    </div>
+  );
+}
+
+export default App;
