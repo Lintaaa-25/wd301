4fa13ec5f07f0d1e1f605161f8eae817f4c770ee
Lintaaa-25 <lintababu3@gmail.com>
2025-05-10 22:18:20 +0530

    Update App.tsx

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,13 +1,51 @@
 import React from "react";
-import TaskApp from "./TaskApp";
-import "./index.css"; // Tailwind CSS entry
+import "./index.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <TaskApp />
-    </div>
-  );
-}
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
+import Layout from "./Layout";
+import TaskDetailsPage from "./pages/TaskDetailsPage";
+import Signin from "./pages/Signin";
+import ProtectedRoute from "./ProtectedRoute";
+import NotFound from "./pages/Notfound";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/signin" replace />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
+    children: [
+      { path: "/home", element: <HomePage /> },
+      { path: "/tasks", element: <TaskListPage /> },
+      { path: "/tasks/:id", element: <TaskDetailsPage /> },
+    ],
+  },
+  {
+    path: "/notfound",
+    element: <NotFound />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/notfound" replace />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
