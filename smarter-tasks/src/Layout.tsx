2616eb72f9fd3a2f8181278d3207ee67cb6f4189
Lintaaa-25 <lintababu3@gmail.com>
2025-05-24 23:32:01 +0530

    Update Layout.tsx

--- smarter-tasks/src/Layout.tsx
+++ smarter-tasks/src/Layout.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { TaskItem } from "./types";
 import Header from "./components/Header";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 const Layout = () => {
-  const [tasks, setTasks] = useState<TaskItem[]>([]);
+  const [storedTasks, setStoredTasks] = useLocalStorage<TaskItem[]>("tasks", []);
+  const [tasks, setTasks] = useState<TaskItem[]>(storedTasks);
   const location = useLocation();
 
   const hideHeader = location.pathname === "/notfound";
