b6539082b8486c2188a6aba700a57d8de5f42bae
Lintaaa-25 <lintababu3@gmail.com>
2025-05-25 18:22:00 +0530

    Update Task.tsx

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,24 +1,34 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { TaskItem } from "./types";
 import "./TaskCard.css";
 
 interface TaskProps {
-  title: string;
-  description: string;
-  dueDate: string;
+  item: TaskItem;
+  removeTask: (task: TaskItem) => void;
 }
 
-class Task extends React.Component<TaskProps> {
-  render() {
-    const { title, description, dueDate } = this.props;
-    return (
-      <div className="TaskItem shadow-md border border-slate-100">
-        <h3 className="text-base font-bold my-1">
-          {title} ({dueDate})
-        </h3>
-        <p className="text-sm text-slate-500">{description}</p>
+const Task = ({ item, removeTask }: TaskProps) => {
+  return (
+    <div className="TaskItem shadow-md border border-slate-100">
+      <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+        <div>
+          <Link to={`/tasks/${item.id}`}>
+            <h2 className="text-base font-bold my-1">{item.todoTitle}</h2>
+          </Link>
+          <p className="text-sm text-slate-500">{item.todoDueDate}</p>
+          <p className="text-sm text-slate-500">Description: {item.todoDescription}</p>
+        </div>
+
+        <button
+          className="deleteTaskButton cursor-pointer flex items-center justify-center h-4 w-4 rounded-full my-5 mr-5"
+          onClick={() => removeTask(item)}
+          aria-label={`Delete task ${item.todoTitle}`}
+        >
+          X
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Task;
