bfce5ed515c19e07f4e17771719f7468a7986686
Lintaaa-25 <lintababu3@gmail.com>
2025-05-25 18:24:14 +0530

    Update Notfound.tsx

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -9,7 +9,7 @@ const NotFound = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center">
+    <div className="flex flex-col items-center justify-center h-screen text-center px-4">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
       <p className="mb-6">Oops! The page you're looking for doesn't exist.</p>
       <button
