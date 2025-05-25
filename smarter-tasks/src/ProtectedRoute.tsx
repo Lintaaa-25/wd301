440ac5328c4809ebfba9f5f009cfe0a905c41ce8
Lintaaa-25 <lintababu3@gmail.com>
2025-05-25 23:36:12 +0530

    Update ProtectedRoute.tsx

--- smarter-tasks/src/ProtectedRoute.tsx
+++ smarter-tasks/src/ProtectedRoute.tsx
@@ -1,10 +1,10 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const isAuthenticated = localStorage.getItem("authenticated") === "true";
-  if (authenticated === "true") {
+  const authenticated = localStorage.getItem("authenticated");
+  if (authenticated === 'true') {
     return <>{children}</>;
   } else {
-    return <Navigate to="/signin" replace />;
-  }
+    return <Navigate to="/signin" />;
+ }
 }
