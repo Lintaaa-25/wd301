863576a1408e3155ef5271a31bd2d08cf3d2e0a0
Lintaaa-25 <lintababu3@gmail.com>
2025-05-26 00:12:56 +0530

    Update Header.tsx

--- smarter-tasks/src/components/Header.tsx
+++ smarter-tasks/src/components/Header.tsx
@@ -15,9 +15,14 @@ const Header = () => {
             <h2 className="text-white text-lg font-bold">Smarter Tasks</h2>
           </div>
           <div className="flex items-center w-1/3 justify-end">
-            <a href="/signin" className="ml-6 text-gray-300 hover:text-white">
-             Signout
+            <a
+              href="/signin"
+              className="ml-6 text-gray-300 hover:text-white"
+              onClick={() => localStorage.setItem("authenticated", "false")}
+            >
+              Signout
             </a>
+
          </div>
         </div>
       </div>
